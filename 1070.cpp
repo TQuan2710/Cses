c0428155fe2d80366305378a963d40abe7eeded6
TQuan <blackcat27102003@gmail.com>
2024-01-25 09:54:21 +0700

    Introductory Problems

--- 1070.cpp
+++ 1070.cpp
@@ -8,11 +8,12 @@ ll n;
 int main()
 {
     cin >> n;
-    if(n < 4 && n != 1) cout << "NO SOLUTION";
+    if(n < 4 && n > 1) cout << "NO SOLUTION"; // the min distance that we can make is 1,not for n equal to 1
+    else if (n == 4) cout << "2 4 1 3"; // special case
     else
     {
-        for(ll i = 2;i <= n;i += 2) cout << i << " ";
         for(ll i = 1;i <= n;i += 2) cout << i << " ";
+        for(ll i = 2;i <= n;i += 2) cout << i << " ";
     }
     return 0;
 }
